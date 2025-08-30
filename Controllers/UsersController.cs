2f7521f2a8cbe484c6c257a0569dbc6ec9137922
Reza Zare <rezasingle.org@gmail.com>
2025-08-30 22:59:54 +0330

    #7 - Add SideBar && MyTickets to the project

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using HelpDesk.Data;
+using HelpDesk.Entities;
+using HelpDesk.ViewModels.UsersDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Controllers
+{
+    public class UsersController(UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<ApplicationUser> signInManager,
+            ApplicationDbContext context)
+        : AuthorizeBaseController
+    {
+
+
+        public async Task<IActionResult> Index()
+        {
+            var usere = await context.Users.ToListAsync();
+            return View(usere);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserDto model)
+        {
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
---
+++ ViewModels/UsersDto/CreateUserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace HelpDesk.ViewModels.UsersDto
+{
+    public class CreateUserDto
+    {
+        //TODO : Create new User
+    }
+}
